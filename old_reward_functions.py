c34d356751794feb4f1d8757e0cd9601040526fe
Verdon Crann <verdon.crann@manchester.ac.uk>
2023-11-01 14:18:25 +0000

    updated sac hyperparameters and reward function

--- old_reward_functions.py
+++ old_reward_functions.py
@@ -141,3 +141,34 @@
 # )  # Attraction to landing point
 
 # reward -= np.abs(obs[3])  # Making it not want to tilt too much
+
+
+# if collided:  # new test reward
+#     self.done = True
+#     if (
+#         landed
+#         and abs(self.drone.velocity[0]) < 1
+#         and abs(self.drone.velocity[1]) < 0.5
+#         and abs(self.drone.attitude) < 15 * (np.pi / 180)
+#     ):
+#         if (
+#             abs(self.drone.position_m[0] - self.y_target_m)
+#             < self.target_radius_m
+#         ):
+#             reward += 2000
+#         else:
+#             reward += 10
+
+#     elif landed:
+#         reward += 1000 * np.exp(
+#             -5
+#             * (
+#                 np.abs(obs[4])
+#                 + np.abs(obs[0]) * 2
+#                 + np.abs(obs[1])
+#                 + np.abs(obs[3])
+#             )
+#             / 5
+#         )
+#     else:
+#         reward += -50
--- turbolander_2d_env_v1.py
+++ turbolander_2d_env_v1.py
@@ -110,63 +110,53 @@ class TurboLander2DEnvV1(gym.Env):
         obs = self.get_observation()
         collided, landed = self.drone.check_collision(self.walls)
 
-        # if collided:  # new test reward
-        #     self.done = True
-        #     if (
-        #         landed
-        #         and abs(self.drone.velocity[0]) < 1
-        #         and abs(self.drone.velocity[1]) < 0.5
-        #         and abs(self.drone.attitude) < 15 * (np.pi / 180)
-        #     ):
-        #         if (
-        #             abs(self.drone.position_m[0] - self.y_target_m)
-        #             < self.target_radius_m
-        #         ):
-        #             reward += 2000
-        #         else:
-        #             reward += 10
+        if collided:  # new test reward for sac model 10
+            self.done = True
+            if (  # Criteria for safe landing
+                landed
+                and abs(self.drone.velocity[0]) < 0.2
+                and abs(self.drone.velocity[1]) < 0.5
+                and abs(self.drone.attitude) < 15 * (np.pi / 180)
+            ):
+                if (  # Criteria for landing on target
+                    abs(self.drone.position_m[0] - self.y_target_m)
+                    <= self.target_radius_m
+                ):
+                    reward += 50 - (
+                        self.current_time_step * 0.1
+                    )  # Safe landing on target
+                else:
+                    reward += 0  # Safe landing off target
+            else:
+                reward += -50  # Crash
 
-        #     elif landed:
-        #         reward += 1000 * np.exp(
-        #             -5
-        #             * (
-        #                 np.abs(obs[4])
-        #                 + np.abs(obs[0]) * 2
-        #                 + np.abs(obs[1])
-        #                 + np.abs(obs[3])
-        #             )
-        #             / 5
-        #         )
-        #     else:
-        #         reward += -50
+            # Landing reward
+            # if collided:  # Used for SAC model 9
+            #     self.done = True
+            #     # reward += -100
+            #     if landed:
+            #         reward += 50 * np.exp(
+            #             -10
+            #             * (
+            #                 np.abs(obs[4])
+            #                 + np.abs(obs[0]) * 2
+            #                 + np.abs(obs[1])
+            #                 + np.abs(obs[3])
+            #             )
+            #             / 5
+            #         )
+            #     else:
+            #         reward += -50
 
-        # Landing reward
-        # if collided:  # Used for model 54
-        #     self.done = True
-        #     # reward += -100
-        #     if landed:
-        #         reward += 1000 * np.exp(
-        #             -10
-        #             * (
-        #                 np.abs(obs[4])
-        #                 + np.abs(obs[0]) * 2
-        #                 + np.abs(obs[1])
-        #                 + np.abs(obs[3])
-        #             )
-        #             / 5
-        #         )
-        #     else:
-        #         reward += -100
-
-        reward += np.exp(
-            -5 * (np.abs(obs[4]) + np.abs(obs[5]))
-        )  # Attraction to landing point (used for model 22 + 53), used as first step for training without penalty
+        # reward += 0.1 * np.exp(
+        #     -5 * (np.abs(obs[4]) + np.abs(obs[5]))
+        # )  # Attraction to landing point (used for model 22 + 53), used as first step for training without penalty
 
         # Stops episode, when drone is out of range or overlaps
         if np.abs(obs[3]) == 1 or np.abs(obs[6]) == 1 or np.abs(obs[7]) == 1:
             self.done = True
-            reward += -100
-            # reward = -10
+            reward += -50
+            # reward = -100
 
         # reward -= 0.25  # Penalty for each time step used for models 35 + 36 and 46, 47, 48 and 49
 
